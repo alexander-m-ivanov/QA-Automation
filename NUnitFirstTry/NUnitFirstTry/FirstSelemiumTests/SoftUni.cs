0b873fd2bbd69a06bd581c59e8339bde096737b8
alexander-m-ivanov <sasho_matrix@abv.bg>
2017-03-28 00:02:01 +0300

    1/3 of the first 3 hour Selenium Video
    Done NUnit and Opening Chrome and entering the http://www.softuni.bg
    website.

---
+++ NUnitFirstTry/NUnitFirstTry/FirstSelemiumTests/SoftUni.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstSelemiumTests
+{
+    [TestFixture]
+    public class SoftUni
+    {
+        [Test]
+        public void EnterSoftUni()
+        {
+            IWebDriver initChrom = new ChromeDriver();
+
+            initChrom.Url = "http://www.softuni.bg";
+
+            initChrom.Quit();
+        }
+    }
+}
---
+++ NUnitFirstTry/NUnitFirstTry/NUnitFirstTry/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApplication1;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitFirstTry
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void BankAccountDepositWithPositiveValue()
+        {
+            BankAcount acc = new BankAcount(3333m);
+            Assert.AreEqual(2222, acc.Amount, "Amount must be 3333");
+        }
+    }
+}
