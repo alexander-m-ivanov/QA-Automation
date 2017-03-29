5d936f724ce2d01cb948e5217a0e478cc18754f0
alexander-m-ivanov <sasho_matrix@abv.bg>
2017-03-29 12:23:56 +0300

    2/3 of the First Actual Selenium Video
    QA Automation course 2017

---
+++ NUnitFirstTry/NUnitFirstTry/FirstSelemiumTests/DemoQATests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstSelemiumTests
+{
+    [TestFixture]
+    class DemoQaTests
+    {
+        public void RegisterWithValidData()
+        {
+            IWebDriver demoTesWebDriver = new ChromeDriver();
+            demoTesWebDriver.Manage().Window.Maximize();
+            demoTesWebDriver.Url = "http://www.demoqa.com/";
+            IWebElement regButton = demoTesWebDriver.FindElement(By.Id("menu-item-374"));
+            regButton.Click();
+            IWebElement regFirstName = demoTesWebDriver.FindElement(By.Id("name_3_firstname"));
+            Type(regFirstName, "Ivan");
+            IWebElement regLasttName = demoTesWebDriver.FindElement(By.Id("name_3_lastname"));
+            Type(regFirstName, "Petrov");
+        }
+
+        private void Type(IWebElement element, string text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+        }
+    }
+}
--- NUnitFirstTry/NUnitFirstTry/FirstSelemiumTests/SoftUni.cs
+++ NUnitFirstTry/NUnitFirstTry/FirstSelemiumTests/SoftUni.cs
@@ -6,20 +6,49 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace FirstSelemiumTests
 {
     [TestFixture]
     public class SoftUni
     {
-        [Test]
         public void EnterSoftUni()
         {
-            IWebDriver initChrom = new ChromeDriver();
+        }
 
-            initChrom.Url = "http://www.softuni.bg";
+        [Test]
+        public void LoginSoftUni()
+        {
+            IWebDriver testInBrowser = new ChromeDriver();
+            testInBrowser.Manage().Window.Maximize();
+            testInBrowser.Url = "http://www.softuni.bg";
+            IWebElement loginButton =
+                testInBrowser.FindElement(By.XPath("//*[@id=\"header-nav\"]/div[2]/ul/li[2]/span/a"));
+            loginButton.Click();
+            IWebElement userName = testInBrowser.FindElement(By.Name("username"));
+            userName.Clear();
+            userName.SendKeys("xanderbg");
+            IWebElement password = testInBrowser.FindElement(By.Name("password"));
+            password.Clear();
+            password.SendKeys("880101");
+            IWebElement enterButton =
+                testInBrowser.FindElement(By.XPath("/html/body/div[2]/div[2]/div[2]/div[2]/div[1]/form/input[2]"));
+            enterButton.Click();
 
-            initChrom.Quit();
+            WebDriverWait wait = new WebDriverWait
+                                 (testInBrowser, TimeSpan.FromSeconds(60));
+            IWebElement logo = wait.Until<IWebElement>
+            ((w) => {
+                return w.FindElement(By.XPath("//*[@id=\"page-header\"]/div[1]/div/div/div[1]/a/img[1]"));
+            });
+
+
+            //IWebElement logo =
+            //    testInBrowser.FindElement(By.XPath("//*[@id=\"page-header\"]/div[1]/div/div/div[1]/a/img[1]"));
+
+            Assert.IsTrue(logo.Displayed);
+            testInBrowser.Quit();
         }
     }
 }
