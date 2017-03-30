18ca1df8e003557402c4b27a87c72997127165f7
alexander-m-ivanov <sasho_matrix@abv.bg>
2017-03-31 00:04:31 +0300

    Beginning of th 3/3 part of the video
    QA Automation course 2017

--- NUnitFirstTry/NUnitFirstTry/FirstSelemiumTests/DemoQATests.cs
+++ NUnitFirstTry/NUnitFirstTry/FirstSelemiumTests/DemoQATests.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace FirstSelemiumTests
 {
     [TestFixture]
     class DemoQaTests
     {
+        [Test]
         public void RegisterWithValidData()
         {
             IWebDriver demoTesWebDriver = new ChromeDriver();
@@ -23,6 +25,18 @@ namespace FirstSelemiumTests
             Type(regFirstName, "Ivan");
             IWebElement regLasttName = demoTesWebDriver.FindElement(By.Id("name_3_lastname"));
             Type(regFirstName, "Petrov");
+            IWebElement maritalStatus = demoTesWebDriver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[2]/div/div/input[1]"));
+            maritalStatus.Click();
+            List<IWebElement> hobbys = demoTesWebDriver.FindElements(By.Name("checkbox_5[]")).ToList();
+            hobbys[0].Click();
+            hobbys[1].Click();
+
+            //тестване на drop-down лист
+            IWebElement country = demoTesWebDriver.FindElement(By.Id("dropdown_7"));
+            SelectElement countryOption = new SelectElement(country);
+            countryOption.SelectByText("Bulgaria");
+
+            demoTesWebDriver.Quit();
         }
 
         private void Type(IWebElement element, string text)
