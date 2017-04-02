feab13a9a9e0a95cf99a257986b85a24f92f030e
alexander-m-ivanov <sasho_matrix@abv.bg>
2017-04-03 00:19:24 +0300

    Finished 1st homework without one exercise
    QA Automation course - 2017

--- 1st homework/1st homework/Selenium tests.cs
+++ 1st homework/1st homework/Selenium tests.cs
@@ -30,14 +30,169 @@ namespace _1st_homework
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
             List<IWebElement> seleniumLink = driver.FindElements(By.XPath("//h3/a")).ToList();
             seleniumLink[0].Click();
-            string elementHtml = seleniumLink[0].Text;
             
-
-
-
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
             string url = driver.Url;
             Assert.IsTrue(url == "http://www.seleniumhq.org/");
+            Assert.AreEqual("Selenium - Web Browser Automation", driver.Title);
+            driver.Quit();
+        }
+
+        [Test]
+        public void QaAutomatoin()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.softuni.bg";
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            IWebElement navigateMenu = driver.FindElement(By.XPath("//*[@id=\"header-nav\"]/div[1]/ul/li[2]/a/span"));
+            navigateMenu.Click();
+
+            IWebElement clickOnCourse =
+                driver.FindElement(
+                    By.XPath("//*[@id=\"header-nav\"]/div[1]/ul/li[2]/div/div/div/div[2]/div[2]/ul/li[6]/a"));
+            clickOnCourse.Click();
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            IWebElement heading1 =
+                driver.FindElement(By.ClassName("page-title"));
+            string heading1Text = heading1.Text;
+
+            IWebElement heading2 =
+                driver.FindElement(By.ClassName("content-title"));
+            string heading2Text = heading1.Text;
+
+            Assert.AreEqual(heading1Text, heading2Text);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DemoQaRegistrationPage()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.demoqa.com";
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            IWebElement registrationButton = driver.FindElement(By.XPath("//*[@id=\"menu-item-374\"]/a"));
+            registrationButton.Click();
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            string url = driver.Url;
+
+            Assert.IsTrue(url == "http://demoqa.com/registration/");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DemoQa_1LastNameRequiered()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://demoqa.com/registration/";
+
+            IWebElement submitRegButton = driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[14]/div/input"));
+            submitRegButton.Click();
+
+            IWebElement validationMessage =
+                driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[1]/div[1]/div[2]/span"));
+            string validationMessageText = validationMessage.Text;
+
+            Assert.IsTrue(validationMessageText == "* This field is required");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DemoQa_2PhoneNumberDigits()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://demoqa.com/registration/";
+
+            IWebElement phoneNumberField = driver.FindElement(By.XPath("//*[@id=\"phone_9\"]"));
+            phoneNumberField.SendKeys("123");
+
+            IWebElement submitRegButton = driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[14]/div/input"));
+            submitRegButton.Click();
+
+            IWebElement validationMessage =
+                driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[6]/div/div/span"));
+            string validationMessageText = validationMessage.Text;
+
+            Assert.IsTrue(validationMessageText == "* Minimum 10 Digits starting with Country Code");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DemoQa_3InvalidMailAddress()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://demoqa.com/registration/";
+
+            IWebElement mailAddressField = driver.FindElement(By.XPath("//*[@id=\"email_1\"]"));
+            mailAddressField.SendKeys("123");
+
+            IWebElement submitRegButton = driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[14]/div/input"));
+            submitRegButton.Click();
+
+            IWebElement validationMessage =
+                driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[8]/div/div/span"));
+            string validationMessageText = validationMessage.Text;
+
+            Assert.IsTrue(validationMessageText == "* Invalid email address");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DemoQa_4PassNotEnoughSymbols()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://demoqa.com/registration/";
+
+            IWebElement passwordField = driver.FindElement(By.Id("password_2"));
+            passwordField.SendKeys("123");
+
+            IWebElement clickOnEmptySpace = driver.FindElement(By.Id("pie_register"));
+            clickOnEmptySpace.Click();
+
+            IWebElement validationMessage =
+                driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[11]/div/div/span"));
+            string validationMessageText = validationMessage.Text;
+
+            Assert.IsTrue(validationMessageText == "* Minimum 8 characters required");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void DemoQa_5PassDoNotMatch()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://demoqa.com/registration/";
+
+            IWebElement passwordField = driver.FindElement(By.Id("password_2"));
+            passwordField.SendKeys("12345678");
+
+            IWebElement passwordConfirmField = driver.FindElement(By.Id("confirm_password_password_2"));
+            passwordConfirmField.SendKeys("123456789");
+
+            IWebElement clickOnEmptySpace = driver.FindElement(By.Id("pie_register"));
+            clickOnEmptySpace.Click();
+
+            IWebElement validationMessage =
+                driver.FindElement(By.XPath("//*[@id=\"pie_register\"]/li[12]/div/div/span"));
+            string validationMessageText = validationMessage.Text;
+
+            Assert.IsTrue(validationMessageText == "* Fields do not match");
 
             driver.Quit();
         }
